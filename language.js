c02fad236c9c083e9036851945a23cc725f3a66c
Fraktalai <WWoras@gmail.com>
2019-07-04 10:20:22 +0300

    4th attempt

--- language.js
+++ language.js
@@ -6,7 +6,7 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 function redirect(lang) {
-	window.location.assign(lang+window.location.pathname.slice(3));
+	window.location.assign("../"+lang+window.location.pathname.slice(3));
 }
 
 function setEnglish(){
@@ -18,3 +18,5 @@ function setLithuanian(){
 	setCookie("language",window.location.pathname,360);
 	redirect("lt");
 }
+
+setCookie("window_loc",window.location.pathname.slice(3),360);
