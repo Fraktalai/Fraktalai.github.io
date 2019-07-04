2fae8af93125591dc9557f8d370194b3c5681016
Fraktalai <WWoras@gmail.com>
2019-07-04 09:47:54 +0300

    First attempt to implement language switching

--- language.js
+++ language.js
@@ -1,1 +1,20 @@
-var language = 0 //0 - lithuanian; 1 - english; 2 - other
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + "path=/";
+}
+
+function redirect(lang) {
+	window.location.assign(window.location.protocol+window.location.hostname+"/"+lang+window.location.pathname.slice(3));
+}
+
+function setEnglish(){
+	setCookie("language","en",360);
+	redirect("en");
+}
+
+function setLithuanian(){
+	setCookie("language",window.location.pathname,360);
+	redirect("lt");
+}
--- signalai.js
+++ signalai.js
@@ -2,8 +2,15 @@ var ctx = document.getElementById("signal1").getContext('2d');
 global = {
 	density: 80} //pikselių skaičius per sekundę
 var save_signal = [];
+dazniai = rand(10, 1, 20);
+fazes = rand(10, 0, 2*Math.PI);
+vieneto_plotis = 100; // Kiek pikselių yra 1 sekundė;
+x1 = 10;
+y1 = 10;
+x2 = 470;
+y2 = 210;
 
-function axis(x1, y1, x2, y2) {
+function axis() {
 	ctx.beginPath();
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x1, y2);
@@ -21,10 +28,10 @@ function axis(x1, y1, x2, y2) {
 	ctx.font = 'bold 14px Arial';
 	for (i = 0; i < 5; i++) {
 		ctx.beginPath();
-		ctx.moveTo(x1+i*100, y2);
-		ctx.lineTo(x1+i*100, y2+5);
+		ctx.moveTo(x1+i*vieneto_plotis, y2);
+		ctx.lineTo(x1+i*vieneto_plotis, y2+5);
 		ctx.stroke();
-		ctx.fillText(i, x1+i*100, y2+20); }
+		ctx.fillText(i, x1+i*vieneto_plotis, y2+20); }
 	ctx.fillText('t', x2, y2+20)
 	ctx.stroke();
 }
@@ -34,18 +41,8 @@ function rand(n, y1, y2) {
 	for (i=0; i<n; i++) y[i]= y1+Math.random()*(y2-y1);
 	return y;
 }
-
-function signalas(freq, fazes, x, y, width, height) {
-	ctx.beginPath();
-	for (p=0; p<width; p++) {
-		for (i=0; i<freq.length; i++) {
-			funcsin[p] += Math.sin(freq[i]*2*Math.PI*(p/global.density)+(fazes[i]/freq[i])); }
-		ctx.lineTo(x+p,y+(height/2)-funcsin[p]*height/2); }
-	ctx.stroke();
-	return funcsin;
-}
 	
-function signal(freq, fazes, x, y, width, height) {
+function tolydus(x, y, width, height) {
 	ctx.beginPath();
 	for (p=0; p<width; p++) {
 		funcsin = 0;
@@ -54,7 +51,19 @@ function signal(freq, fazes, x, y, width, height) {
 		ctx.lineTo(x+p,y+(height/2)-funcsin*height/2); }
 	ctx.stroke();
 }
+
+function diskretus(x, y, width, height) {
+	ctx.beginPath();
+	for (p=0; p<width; p++) {
+		funcsin = 0;
+		for (i=0; i<freq.length; i++) {funcsin += Math.sin(freq[i]*2*Math.PI*p/width+fazes[i]);}
+		save_signal[p]=funcsin;
+		ctx.lineTo(x+p,y+(height/2)-funcsin*height/2); }
+	ctx.stroke();
+}
+
 	
-axis(10, 10, 470, 210);
+axis();
 //signal(20, 20, 440, 120);
-signal(rand(10, 1, 20), rand(10, 0, 2*Math.PI), 10, 110, 460, 25);
+tolydus(10, 110, 460, 25);
+diskretus(10, 110, 460, 25);
